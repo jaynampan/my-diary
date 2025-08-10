2e5f2c0c0df40ae01b04a40cddca8966b82359d7
jaynampan <jaynamwang@outlook.com>
2025-08-10 21:52:41 +0800

    feature: add dependency auto checker & update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,6 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.compose.compiler)  apply false
+    // check for dependency updates
+    alias(libs.plugins.dependency.checker) apply false
 }
